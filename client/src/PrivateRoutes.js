46181f6e031fe38a96ce211f573c32398cb1aa75
tsipporahc <tsipporahc@gmail.com>
2022-12-04 17:37:24 -0800

    Update Private Routes and code comments

--- client/src/PrivateRoutes.js
+++ client/src/PrivateRoutes.js
@@ -1,8 +1,12 @@
+/**
+ * This PrivteRoutes components wraps child routes and requires an authenticated User to have access to certain routes.
+ * @param {object} component - Component to be rendered
+ */
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { Consumer } from './Context';
 
-const PrivateRoutes = ({ component: Component, ...rest }) => {
+const PrivateRoutes = () => {
   return (
     <Consumer>
       {(context) =>

5673ca4b4260507dfb18bd6bc68832d8fff8435a
tsipporahc <tsipporahc@gmail.com>
2022-12-07 02:00:56 -0800

    set up for front end deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line
 export default {
-  apiBaseUrl: 'https://https://school-database-backend.up.railway.app/api',
+  apiBaseUrl: 'https://school-database-backend.up.railway.app/api',
 };
